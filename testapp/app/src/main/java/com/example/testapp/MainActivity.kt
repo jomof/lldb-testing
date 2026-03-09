6dd65697ad5c397f8a5266634ef09a01538079b9
Emre Kultursay <emrekultursay@google.com>
2026-03-09 11:41:20 -0700

    Add com.example.testapp

---
+++ testapp/app/src/main/java/com/example/testapp/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.testapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.example.testapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+     binding = ActivityMainBinding.inflate(layoutInflater)
+     setContentView(binding.root)
+
+        // Example of a call to a native method
+        binding.sampleText.text = stringFromJNI()
+    }
+    /**
+      * A native method that is implemented by the 'testapp' native library,
+      * which is packaged with this application.
+      */
+     external fun stringFromJNI(): String
+
+     companion object {
+         // Used to load the 'testapp' library on application startup.
+         init {
+             System.loadLibrary("testapp")
+         }
+     }
+}
