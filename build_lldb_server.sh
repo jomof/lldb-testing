affde2adc9c98e4499d0bafeb42f59fceb3b5a1a
Jomo Fisher <jomof@google.com>
2026-05-07 21:03:09 +0000

    Align names, add android-x86

--- build_lldb_server.sh
+++ build_lldb_server.sh
@@ -27,6 +27,8 @@ elif [[ "$ANDROID_ABI" == "armeabi-v7a" ]]; then
   LLVM_HOST_TRIPLE=arm-unknown-linux-androideabi
 elif [[ "$ANDROID_ABI" == "x86_64" ]]; then
   LLVM_HOST_TRIPLE=x86_64-unknown-linux-android
+elif [[ "$ANDROID_ABI" == "x86" ]]; then
+  LLVM_HOST_TRIPLE=i686-unknown-linux-android
 elif [[ "$ANDROID_ABI" == "riscv64" ]]; then
   LLVM_HOST_TRIPLE=riscv64-unknown-linux-android
 else
